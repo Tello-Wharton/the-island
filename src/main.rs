2279c749b85a5e6a7f21296b6649770e27ff10f6
Tello-Wharton <aaron.tkd@hotmail.com>
2024-01-04 19:43:52 +0000

    sorting out player logging stats

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 
 use std::io;
 
+struct Player {
+    name: String,
+    health: i32
+}
+
 fn main() {
     println!("Enter a string, I'll yell it back. Unless you tell me to quit. Then I will quit");
 
+    let mut player = Player {
+        name: String::from("Player 1"),
+        health: 100
+    };
 
     loop {
         let mut input = String::new();
@@ -13,6 +22,7 @@ fn main() {
             println!("Quitting...");
             break;
         } else {
+            println!("Player name is {}, player health is {}", player.name, player.health);
             println!("You said {}", input);   
         }
     }
