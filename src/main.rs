314b2b1d7efdf5befc5ccd7cdf41b7dfe66855da
Tello-Wharton <aaron.tkd@hotmail.com>
2024-01-04 19:07:43 +0000

    echoing and quitting added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a string, I'll yell it back. Unless you tell me to quit. Then I will quit");
+
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        if input.trim().to_lowercase() == "quit" {
+            println!("Quitting");
+            break;
+        } else {
+            println!("You said {}", input.to_uppercase());   
+        }
+    }
+
 }
