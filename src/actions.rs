4278cad651fdad32c23e4edb6e765583d6e3de00
Tello-Wharton <aaron.tkd@hotmail.com>
2024-01-21 13:41:27 +0000

    refactored move command

--- src/actions.rs
+++ src/actions.rs
@@ -1,8 +1,26 @@
 
+trait ActionsForString {
+
+    fn get_action_and_remaining<'a, 'b>(&'a self, prefix: &'b str) -> Option<(&'b str, &'a str)>;
+
+}
+
+impl ActionsForString for str {
+
+    fn get_action_and_remaining<'a, 'b>(&'a self, prefix: &'b str) -> Option<(&'b str, &'a str)> {
+        if let Some(remaining) = self.strip_prefix(prefix) {
+            Some((prefix, remaining))
+        } else {
+            None
+        }
+    }
+
+}
+
 pub fn get_action(input: &str) {
 
-    if let Some(remaining) = input.strip_prefix("move ") {
-        println!("Time to move {}", remaining);
+    if let Some((prefix, remaining)) = input.get_action_and_remaining("move ") {
+        println!("Prefix: {}, Remaining: {}", prefix, remaining);
     } 
     else if input == "quit" {
         println!("Quitting...");
