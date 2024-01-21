5257ec7e49af947b625dc513068338bb9ae8366c
Tello-Wharton <aaron.tkd@hotmail.com>
2024-01-21 15:17:46 +0000

    refactored get_action_and_remaining

--- src/actions.rs
+++ src/actions.rs
@@ -1,45 +1,48 @@
+use std::fmt::Display;
 
 struct Action {
+    name: &'static str,
     prefix: &'static str,
 }
 
-static  ACTIONS: [Action; 1] = [
-    Action {
-        prefix: "move ",
-    },
+impl Display for Action {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+static ACTIONS: [Action; 1] = [
+    Action { 
+        name: "Move",
+        prefix: "move " 
+    }
 ];
 
 trait ActionsForString {
-
-    fn get_action_and_remaining<'a, 'b>(&'a self, prefix: &'b str) -> Option<(&'b str, &'a str)>;
-
+    fn get_action_and_remaining(&self, prefix: &Action) -> Option<(&str)>;
 }
 
 impl ActionsForString for str {
-
-    fn get_action_and_remaining<'a, 'b>(&'a self, prefix: &'b str) -> Option<(&'b str, &'a str)> {
-        if let Some(remaining) = self.strip_prefix(prefix) {
-            Some((prefix, remaining))
+    fn get_action_and_remaining(&self, action: &Action) -> Option<(&str)> {
+        if let Some(remaining) = self.strip_prefix(action.prefix) {
+            Some(remaining)
         } else {
             None
         }
     }
-
 }
 
 pub fn get_action(input: &str) {
-
     for action in ACTIONS.iter() {
-        if let Some((prefix, remaining)) = input.get_action_and_remaining(action.prefix) {
-            println!("Prefix: {}, Remaining: {}", prefix, remaining);
+        if let Some(remaining) = input.get_action_and_remaining(&action) {
+            println!("Action: {}, Remaining: {}", action, remaining);
             return;
         }
     }
 
     if input == "quit" {
         println!("Quitting...");
-    } 
-    else {
+    } else {
         println!("I don't understand {}", input);
     }
 }
